6925fc7fb567fd1d841ca548181b9cd1423ff7cf
aeddakhly <ayoub.eddakhly@carians.fr>
2020-12-02 16:26:32 +0100

    change conf dir

--- src/scripts/generate_config_files.sh
+++ src/scripts/generate_config_files.sh
@@ -8,13 +8,13 @@ do
     for j in "${STAGES[@]}"
     do
         echo $i.$j
-        envsubst < /etc/nginx/user.conf.d/nginx-template | sed -e 's/§/$/g' > /etc/nginx/user.conf.d/$i.$j.conf
-	    sed -i "s/%PLATFORM%/$i/g" /etc/nginx/user.conf.d/$i.$j.conf
-	    sed -i "s/%STAGE%/.$j/g" /etc/nginx/user.conf.d/$i.$j.conf
+        envsubst < /etc/nginx/conf.d/nginx-template | sed -e 's/§/$/g' > /etc/nginx/conf.d/$i.$j.conf
+	    sed -i "s/%PLATFORM%/$i/g" /etc/nginx/conf.d/$i.$j.conf
+	    sed -i "s/%STAGE%/.$j/g" /etc/nginx/conf.d/$i.$j.conf
     done
-    envsubst < /etc/nginx/user.conf.d/nginx-template | sed -e 's/§/$/g' > /etc/nginx/user.conf.d/$i.conf
-    sed -i "s/%PLATFORM%/$i/g" /etc/nginx/user.conf.d/$i.conf
-    sed -i "s/%STAGE%//g" /etc/nginx/user.conf.d/$i.conf
+    envsubst < /etc/nginx/conf.d/nginx-template | sed -e 's/§/$/g' > /etc/nginx/conf.d/$i.conf
+    sed -i "s/%PLATFORM%/$i/g" /etc/nginx/conf.d/$i.conf
+    sed -i "s/%STAGE%//g" /etc/nginx/conf.d/$i.conf
 done
 
 echo "Goodbye!"
